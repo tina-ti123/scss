5f7a10d66de0b3b7628e88e02c1cc52f2e54f2c3
tina-ti123 <72881642+tina-ti123@users.noreply.github.com>
2020-11-09 14:32:39 +0400

    js

---
+++ index.js
@@ -0,0 +1,22 @@
+let admin ="name"
+let name ="nebiemieri"
+
+console.log(name)
+
+let name ="david"
+console.log('hello ${1}');//
+console.log('hello ${name}');//
+console.log('hello ${name}');//
+
+let x = true
+
+if(x < 50)
+{console.log(true)} else {console.log(number is not corect)}
+if ( x > 50 )
+ {console.log(true)}
+ else {console.log(number is not corect)}
+ if ( x==50 )
+ {console.log(true) } else {console.log (number is not corect)}
+
+ 
+  
