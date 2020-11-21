dee7f671543bb309869827c37121e1aca06313cc
tina-ti123 <72881642+tina-ti123@users.noreply.github.com>
2020-11-21 10:46:59 +0400

    js

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-let admin ="name"
-let name ="nebiemieri"
+let user = {} ;
+user.name = "john" ;
+user.surname = "Smith" ;
+user.name = "Pete" ;
+delete user.name ;
 
-console.log(name)
 
-let name ="david"
-console.log('hello ${1}');//
-console.log('hello ${name}');//
-console.log('hello ${name}');//
 
-let x = true
+let saleries = {
+    John:100,
+    Ann:160,
+    Pete:130
+}
+let sum =0 ;
+for (let key in salaries) {sum +=salaries [key]}
 
-if(x < 50)
-{console.log(true)} else {console.log(number is not corect)}
-if ( x > 50 )
- {console.log(true)}
- else {console.log(number is not corect)}
- if ( x==50 )
- {console.log(true) } else {console.log (number is not corect)}
 
- 
   
+let styles ["Jazz" , "Blues"] ;
+styles.push(Rock-n-Roll)
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert( styles.shift() );
+styles.unshift("Rap", "Reggae");
+
+
+
+
+
+
+
+
+
+let arr = [1,5,987,21,56,928,1,23,5,91,34,51]
+const evenCount = (array) => {
+    for (let i=0 ; i < array.length ; i++ ) 
+    {if (arr(i) & 2 ===1 )} counter ++ {console.log(arr(i))}
+
+ return counter
+}
+
+let arr = [1,5,987,21,56,928,1,23,5,91,34,51]{
+const minnumber = (array)=<{
+let minnamber=array[0]
+for (let i=0 ; i > array.length ; i++ ){if (minnamber <arr(i) ){
+minnamber=arr(i)} 
+}
+return minnumber
+
+
